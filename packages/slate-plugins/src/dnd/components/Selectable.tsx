e9a7bd989d91f304d92f7bbe9cac75ebe0530f03
cjh <1797793818@qq.com>
2021-03-29 10:20:44 +0800

    - 修复 resize bug - 编辑器增加 classname - 修复 draggo button 样式

--- packages/slate-plugins/src/dnd/components/Selectable.tsx
+++ packages/slate-plugins/src/dnd/components/Selectable.tsx
@@ -1,75 +1,50 @@
-import React, { useRef } from 'react';
-import useMergedRef from '@react-hook/merged-ref';
-import Tippy from '@tippyjs/react';
-import { mergeStyles } from '@uifabric/styling';
-import { classNamesFunction, styled } from '@uifabric/utilities';
-import { useDndBlock } from '../hooks/useDndBlock';
-import { grabberTooltipProps } from './grabberTooltipProps';
-import { getSelectableStyles } from './Selectable.styles';
-import {
-  SelectableProps,
-  SelectableStyleProps,
-  SelectableStyles,
-} from './Selectable.types';
+import React, { useRef } from 'react'
+import useMergedRef from '@react-hook/merged-ref'
+import Tippy from '@tippyjs/react'
+import { mergeStyles } from '@uifabric/styling'
+import { classNamesFunction, styled } from '@uifabric/utilities'
+import { useDndBlock } from '../hooks/useDndBlock'
+import { grabberTooltipProps } from './grabberTooltipProps'
+import { getSelectableStyles } from './Selectable.styles'
+import { SelectableProps, SelectableStyleProps, SelectableStyles } from './Selectable.types'
 
-const getClassNames = classNamesFunction<
-  SelectableStyleProps,
-  SelectableStyles
->();
+const getClassNames = classNamesFunction<SelectableStyleProps, SelectableStyles>()
 
-const SelectableBase = ({
-  children,
-  element,
-  className,
-  styles,
-  componentRef,
-  dragIcon,
-}: SelectableProps) => {
-  const blockRef = useRef<HTMLDivElement>(null);
-  const rootRef = useRef<HTMLDivElement>(null);
-  const multiRootRef = useMergedRef(componentRef, rootRef);
+const SelectableBase = ({ children, element, className, styles, componentRef, dragIcon }: SelectableProps) => {
+  const blockRef = useRef<HTMLDivElement>(null)
+  const rootRef = useRef<HTMLDivElement>(null)
+  const multiRootRef = useMergedRef(componentRef, rootRef)
 
   const { dropLine, dragRef, isDragging } = useDndBlock({
     id: element.id,
     blockRef: rootRef,
-  });
+  })
 
-  const dragWrapperRef = useRef(null);
-  const multiDragRef = useMergedRef(dragRef, dragWrapperRef);
+  const dragWrapperRef = useRef(null)
+  const multiDragRef = useMergedRef(dragRef, dragWrapperRef)
 
   const classNames = getClassNames(styles, {
     className,
     direction: dropLine,
     isDragging,
-  });
+  })
 
   return (
     <div className={classNames.root} ref={multiRootRef}>
-      <div
-        ref={blockRef}
-        className={mergeStyles(classNames.blockAndGutter, classNames.block)}
-      >
+      <div ref={blockRef} className={mergeStyles(classNames.blockAndGutter, classNames.block)}>
         {children}
 
-        {!!dropLine && (
-          <div className={classNames.dropLine} contentEditable={false} />
-        )}
+        {!!dropLine && <div className={classNames.dropLine} contentEditable={false} />}
       </div>
 
-      <div
-        className={mergeStyles(
-          classNames.blockAndGutter,
-          classNames.gutterLeft
-        )}
-        contentEditable={false}
-      >
-        <div className={classNames.blockToolbarWrapper}>
+      <div className={mergeStyles(classNames.blockAndGutter, classNames.gutterLeft)} contentEditable={false}>
+        <div className={classNames.blockToolbarWrapper }>
           <div className={classNames.blockToolbar}>
             <Tippy {...grabberTooltipProps}>
               <div ref={multiDragRef}>
                 <button
                   type="button"
-                  className={classNames.dragButton}
+                  className={classNames.dragButton + ' slate-drag-button'}
                   onMouseDown={(e: any) => e.stopPropagation()}
                 >
                   {dragIcon}
@@ -80,13 +55,14 @@ const SelectableBase = ({
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export const Selectable = styled<
-  SelectableProps,
-  SelectableStyleProps,
-  SelectableStyles
->(SelectableBase, getSelectableStyles, undefined, {
-  scope: 'Selectable',
-});
+export const Selectable = styled<SelectableProps, SelectableStyleProps, SelectableStyles>(
+  SelectableBase,
+  getSelectableStyles,
+  undefined,
+  {
+    scope: 'Selectable',
+  },
+)
